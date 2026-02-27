0fdcbbdb06cc0239b239a6038d9eef47a0daefb9
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-28 06:14:30 +0900

    feat: 학생관리 서비스 비즈니스 로직 작성

---
+++ services/students_service.py
@@ -0,0 +1,76 @@
+from fastapi import HTTPException
+from schemas import Student, StudentUpdate
+from db import students_db
+
+
+# 학생 조회(GET)
+def get_students(student_id: str = None, grade: int = None, name: str = None):
+
+    # 학번으로 조회
+    if student_id:
+        matched = [s for s in students_db if s.student_id == student_id]
+        if not matched:
+            raise HTTPException(status_code=404, detail=f"존재하지 않는 학번입니다.")
+        return matched
+
+    # 이름으로 조회
+    if name:
+        matched = [s for s in students_db if s.name == name]
+        if not matched:
+            raise HTTPException(status_code=404, detail=f"학생({name})이 존재하지 않습니다.")
+        return matched
+
+    # 학년별 조회
+    if grade:
+        return [s for s in students_db if s.grade == grade]
+
+    # 전체 조회
+    return students_db
+
+
+# 학생 추가(POST)
+def add_student(student_pk: int, student_id: str, name: str, grade: int, status: str = "재학", role: str = "학생"):
+
+    new_student = Student(
+        student_pk=student_pk,
+        student_id=student_id,
+        name=name,
+        grade=grade,
+        status=status,
+        role=role,
+    )
+    for s in students_db:
+        if s.student_id == student_id:
+            raise HTTPException(status_code=400, detail="이미 존재하는 학번입니다.")
+
+    students_db.append(new_student)
+
+    return {
+        "message": f"{name} 학생({student_id})이 추가되었습니다.",
+        "student_id": student_id,
+    }
+
+
+# 학생 정보 수정(PATCH)
+def update_student(student_pk: int, update_data: StudentUpdate):
+    student = next((s for s in students_db if s.student_pk == student_pk), None)
+    if not student:
+        raise HTTPException(status_code=404, detail=f"PK {student_pk}번 학생을 찾을 수 없습니다.")
+
+    update_dict = update_data.model_dump(exclude_unset=True)
+
+    for key, value in update_dict.items():
+        setattr(student, key, value)
+    return {"message": f"학생 정보가 수정되었습니다.", "student": student}
+
+
+# 학생 삭제(DELETE)
+def delete_student(student_pk: int):
+    student = next((s for s in students_db if s.student_pk == student_pk), None)
+    if not student:
+        raise HTTPException(status_code=404, detail=f"{student_pk}번 학생을 찾을 수 없습니다.")
+
+    else:
+        students_db.remove(student)
+
+    return {"message": f"삭제되었습니다."}
