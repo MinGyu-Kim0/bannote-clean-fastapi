17eedd02d90a484592379359f3bbe3e94794b970
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-28 06:10:19 +0900

    feat: PATCH를 위한 모델 추가 및 대리 키 추가에 따른 필드명 추가/수정

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 from datetime import date
 
 
@@ -8,11 +8,21 @@ from datetime import date
 
 # 학생
 class Student(BaseModel):
+    student_pk: int  # 대리키(PK)
     student_id: str
     name: str
     grade: int
     status: str = "재학"
     role: str = "학생"
+
+
+# 학생 정보 수정
+class StudentUpdate(BaseModel):
+    student_id: Optional[str] = None
+    name: Optional[str] = None
+    grade: Optional[int] = None
+    status: Optional[str] = None
+    role: Optional[str] = None
 
 
 # 청소 구역
@@ -32,7 +42,7 @@ class Schedule(BaseModel):
 class Assignment(BaseModel):
     assignment_id: int
     schedule_id: int
-    student_id: str
+    student_pk: int
     area_id: int
     status: str = "배정"
 
