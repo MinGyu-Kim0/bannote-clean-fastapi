e0f7fda7835a1f9cda59cf62546b2bae332ce20f
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-28 17:28:01 +0900

    feat: schedule 엔드포인트 연결

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from router import students, areas
+from router import students, areas, schedules
 
 app = FastAPI(title="청소 관리 시스템", description="청소 구역 배정 및 관리를 위한 API 서버", version="1.0.0")
 
 app.include_router(students.router)
 app.include_router(areas.router)
+app.include_router(schedules.router)
 
 
 # 헬스 체크
