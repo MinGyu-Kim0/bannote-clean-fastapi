05af0523a51e17c01f8c13b3eb6041fbee0e14d4
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-28 17:02:35 +0900

    feat: 청소 구역 관리 CRUD 로직 구현

---
+++ services/areas_service.py
@@ -0,0 +1,56 @@
+from fastapi import HTTPException
+from schemas import Area, AreaUpdate
+from db import areas_db
+
+
+# 청소 구역 조회
+def get_areas():
+    return areas_db
+
+
+# 청소 구역 추가
+def add_area(area_id: int, name: str, target_grades: list[int]):
+
+    new_area = Area(area_id=area_id, name=name, target_grades=target_grades)
+
+    # db연동 시 삭제할 것(auto increment & PK)
+    if any(a.area_id == area_id for a in areas_db):
+        raise HTTPException(status_code=400, detail="이미 존재하는 구역입니다.")
+
+    # 중복 방지
+    if any(a.name == name for a in areas_db):
+        raise HTTPException(status_code=400, detail="이미 존재하는 구역입니다.")
+
+    areas_db.append(new_area)
+
+    return {"message": f"{name} 추가 되었습니다.", "name": name}
+
+
+# 청소 구역 수정
+def update_area(area_id: int, update_data: AreaUpdate):
+    area = next((a for a in areas_db if a.area_id == area_id), None)
+    if not area:
+        raise HTTPException(status_code=404, detail="찾을 수 없습니다.")
+
+    update_dict = update_data.model_dump(exclude_unset=True)
+
+    if "name" in update_dict:
+        new_name = update_dict["name"]
+        if any(a.name == new_name for a in areas_db):
+            raise HTTPException(status_code=400, detail="해당 이름으로 수정할 수 없습니다.")
+
+    for key, value in update_dict.items():
+        setattr(area, key, value)
+    return {"message": "정보가 수정되었습니다.", "area": area}
+
+
+# 청소 구역 삭제
+def del_area(area_id: int):
+    area = next((a for a in areas_db if a.area_id == area_id), None)
+    if not area:
+        raise HTTPException(status_code=404, detail=f"찾을 수 없습니다.")
+
+    else:
+        areas_db.remove(area)
+
+    return {"message": "삭제되었습니다."}
