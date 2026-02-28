2441af045c7e0d184cd94b20577b0fe52fb9a905
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-28 17:05:44 +0900

    feat: areas 라우터 연결

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from router import students
+from router import students, areas
 
 app = FastAPI(title="청소 관리 시스템", description="청소 구역 배정 및 관리를 위한 API 서버", version="1.0.0")
 
 app.include_router(students.router)
+app.include_router(areas.router)
 
 
 # 헬스 체크
